fc0908e0cd70a3ab053eed1e6544739434d3a35b
bharris62 <blakebharris@gmail.com>
2017-05-28 13:50:45 -0500

    added the ability to add review to course

--- ed_reviews/courses/views.py
+++ ed_reviews/courses/views.py
@@ -1,21 +1,40 @@
-
 # Create your views here.
+from django.shortcuts import get_object_or_404
 from rest_framework import generics
-from rest_framework import status
-from rest_framework.views import APIView
-from rest_framework.response import Response
 
 from . import models
 from . import serializers
 
-class ListCreateCourse(APIView):
-    def get(self, request, format=None):
-        courses = models.Course.objects.all()
-        serializer = serializers.CourseSerializer(courses, many=True)
-        return Response(serializer.data)
+class ListCreateCourse(generics.ListCreateAPIView):
+    queryset = models.Course.objects.all()
+    serializer_class = serializers.CourseSerializer
 
-    def post(self, request, format=None):
-        serializer = serializers.CourseSerializer(data=request.data)
-        serializer.is_valid(raise_exception=True)
-        serializer.save()
-        return Response(serializer.data, status=status.HTTP_201_CREATED)
+class RetrieveUpdateDestroyCourse(generics.RetrieveUpdateDestroyAPIView):
+    queryset = models.Course.objects.all()
+    serializer_class = serializers.CourseSerializer
+
+class ListCreateReview(generics.ListCreateAPIView):
+    queryset = models.Review.objects.all()
+    serializer_class = serializers.ReviewSerializer
+
+    # this gets the queryset (mulitple items), overriding default to use course_pk to get correct filter
+    def get_queryset(self):
+        return self.queryset.filter(course_id=self.kwargs.get('course_pk'))
+
+    # override, this runs when an obj is created by the view.
+    def perform_create(self, serializer):
+        course = get_object_or_404(
+            models.Course, pk=self.kwargs.get('course_pk'))
+        serializer.save(course=course)
+
+class RetrieveUpdateDestroyReview(generics.RetrieveUpdateDestroyAPIView):
+    queryset = models.Review.objects.all()
+    serializer_class = serializers.ReviewSerializer
+
+    # gets 1 item
+    def get_object(self):
+        return get_object_or_404(
+            self.get_queryset(),
+            course_id=self.kwargs.get('course_pk'),
+            pk=self.kwargs.get('pk')
+        )
